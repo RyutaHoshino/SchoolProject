c55f54e03a80e01cba923c63a06eb5ac548e98c0
Ryuta <s102r.h@gmail.com>
2018-03-15 20:16:00 +0900

    fix coflict

--- kadai7.py
+++ kadai7.py
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-from xml.etree import ElementTree
-
-# XML 文字列をパースして Element オブジェクトを生成
-elem = ElementTree.fromstring('<tree name="hello"><trunk>...</trunk></tree>')
-print(elem.tag)     #=> tree
-print(elem.attrib)  #=> {'name': 'hello'}
-=======
 # coding:utf-8
 from xml.dom import minidom
 xmldoc = minidom.parse('sample.xml')
@@ -15,4 +7,3 @@ itemlist = xmldoc.getElementsByTagName('UML:ActionState')
 for s in itemlist:
     print(s.attributes['xmi.id'].value)
     print(s.attributes['name'].value)
->>>>>>> c1c024c5b49085b44741c66041c1d1eb430f2cc4
