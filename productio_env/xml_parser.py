2397b987f9b6262c6990e1f8a7fe766136816927
imura <imura@synapse.am>
2018-03-20 02:42:15 +0900

    沼から抜け出した！

--- productio_env/xml_parser.py
+++ productio_env/xml_parser.py
@@ -11,28 +11,44 @@ output_flow_figure_list = []
 initialData =[]
 
 def extraction_action_state(data_lists):
-    # フロー図のstateのみ抽出します
+    # フロー図のstateのみ抽出します 出発点 starting_point　目的地  destination
     for state_parts in data_lists['UML:Pseudostate']:
-        try:
-            if state_parts['UML:StateVertex.outgoing']:
-                from_xmi = state_parts['UML:StateVertex.outgoing']
-                # ここが動かないので修正します！
-                import pdb; pdb.set_trace()
-            elif state_parts['UML:StateVertex.incoming']:
-                to_xmi = state_parts['UML:StateVertex.incoming']
-        except Exception as e:
-            print(e)
+        starting_point = []
+        destination = []
+        if 'UML:StateVertex.outgoing' in state_parts:
+            # 配列の長さで条件分岐する！ 0 or 1 or 2以上
+            transaction_count = len(state_parts['UML:StateVertex.outgoing']['UML:Transition'])
+            if transaction_count == 1:
+                xml = state_parts['UML:StateVertex.outgoing']['UML:Transition']['@xmi.idref']
+                starting_point.append({ 'from_xmi': xml })
+            else:
+                for state_part in state_parts['UML:StateVertex.outgoing']['UML:Transition']:
+                    starting_point.append({ 'from_xmi': state_part['@xmi.idref'] })
         else:
-            name = urllib.parse.unquote(state_parts['@name'])
-        state_part = { 'root': { 'from_xmi': from_xmi }, 'name': name }
-        state_lists.append(state_part)
+            starting_point.append({ 'from_xmi': '' })
+
+        if 'UML:StateVertex.incoming' in state_parts:
+            transaction_count = len(state_parts['UML:StateVertex.incoming']['UML:Transition'])
+            if transaction_count == 1:
+                xml = state_parts['UML:StateVertex.incoming']['UML:Transition']['@xmi.idref']
+                destination.append({ 'to_xmi': xml })
+            else:
+                for state_part in state_parts['UML:StateVertex.incoming']['UML:Transition']:
+                    starting_point.append({ 'to_xmi': state_part['@xmi.idref'] })
+        else:
+            destination.append({ 'to_xmi': '' })
+        #　統合するよ
+        name = urllib.parse.unquote(state_parts['@name'])
+        import pdb; pdb.set_trace()
+        state_part = { 'root': { starting_point,destination }, 'name': name }
+        # state_lists.append(state_part)
     # 終点部分の処理
-    last_part = urllib.parse.unquote(data_lists['UML:FinalState']['@name'])
-    state_lists.insert(-1, last_part)
-   # 中間部分の処理
-    for action_parts in data_lists['UML:ActionState']:
-        middle_part = urllib.parse.unquote(action_parts['@name'])
-        state_lists.append(middle_part)
+   #  last_part = urllib.parse.unquote(data_lists['UML:FinalState']['@name'])
+   #  state_lists.insert(-1, last_part)
+   # # 中間部分の処理
+   #  for action_parts in data_lists['UML:ActionState']:
+   #      middle_part = urllib.parse.unquote(action_parts['@name'])
+   #      state_lists.append(middle_part)
 
     return state_lists
 
